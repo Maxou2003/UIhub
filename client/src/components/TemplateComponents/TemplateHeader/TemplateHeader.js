7023f76b7afceedc159ca291a9169ece0e853b01
Maxence <lemaila.maxence@gmail.com>
2025-03-31 16:01:05 +0200

    navbar adjustements

--- client/src/components/TemplateComponents/TemplateHeader/TemplateHeader.js
+++ client/src/components/TemplateComponents/TemplateHeader/TemplateHeader.js
@@ -4,7 +4,7 @@ import { IonIcon } from '@ionic/react';
 import { arrowBack } from 'ionicons/icons';
 import Modal from 'react-bootstrap/Modal'
 import api from '../../../utils/api';
-import connected from '../../../utils/connected';
+import { isConnected } from '../../../utils/connected';
 import { useEffect } from 'react';
 
 function TemplateHeader({ owner, label, id }) {
@@ -126,7 +126,7 @@ function TemplateHeader({ owner, label, id }) {
                         {user.username}
                     </button>
                 </div>}
-                {connected && <button className='fork-btn' onClick={handleFork}><span><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                {isConnected() && <button className='fork-btn' onClick={handleFork}><span><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 0h24v24H0z" fill="none"></path>
                     <path
                         d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
