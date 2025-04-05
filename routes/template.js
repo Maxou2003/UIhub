b182c43f65ff95cc32a86e6d645c398e81778480
MisterTigre <mrtigre360@gmail.com>
2025-04-05 22:39:21 +0200

    add label search

--- routes/template.js
+++ routes/template.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth');
 const is_owner = require('../middleware/is_owner');
 
 router.get('/', templateController.getTemplates);
+router.post('/label/', templateController.getTemplateLabel);
 router.get('/:id', templateController.getTemplateWithId);
 router.put('/', auth, is_owner, templateController.uptadeTemplate);
 router.post('/', auth, templateController.saveTemplate);
